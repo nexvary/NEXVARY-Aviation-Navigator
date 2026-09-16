2e1c469f00424df39dda69067574b313d08508a8
NEXVARY Inc <325503741+nexvary@users.noreply.github.com>
2026-09-16 07:24:55 -0700

    feat: centralize NEXVARY social links

---
+++ app/src/main/java/com/nexvary/aviationnavigator/ExternalLinks.kt
@@ -0,0 +1,12 @@
+package com.nexvary.aviationnavigator
+
+object ExternalLinks {
+    const val WEBSITE = "https://nexvary.com/"
+    const val FACEBOOK = "https://www.facebook.com/share/14p9krEn5ij/"
+    const val EMAIL = "mailto:info@nexvary.com"
+    const val YOUTUBE = "https://www.youtube.com/@NexvaryInc"
+    const val X = "https://x.com/Nexvary"
+
+    val webLinks = listOf(WEBSITE, FACEBOOK, YOUTUBE, X)
+    val all = webLinks + EMAIL
+}
