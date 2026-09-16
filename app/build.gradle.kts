9934de8af28d7e623b9398330fcf33e7cd4fdcf1
NEXVARY Inc <325503741+nexvary@users.noreply.github.com>
2026-09-16 05:12:32 -0700

    build: compile against Android API 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.nexvary.aviationnavigator"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.nexvary.aviationnavigator"
